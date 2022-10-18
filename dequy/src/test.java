e3f8fb955285e51e9d31d3bb0ddf141cf425d6dd
huyjaky <huyphanuyg@gmail.com>
2022-10-18 21:20:03 +0700

    testing

---
+++ dequy/src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        String huy = "1234567123";
+        System.out.println(huy.substring(0,2));
+        System.out.println(huy.replace(huy.substring(0,4), ""));
+        System.out.println(huy);
+    }
+}
