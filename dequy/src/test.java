e4c5c68a1c693125e6ddc40bb6d2d1b49643ff68
huyjaky <huyphanuyg@gmail.com>
2022-10-18 21:57:16 +0700

    test thu

--- dequy/src/test.java
+++ dequy/src/test.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
         String huy = "1234567123";
         System.out.println(huy.substring(0,2));
         System.out.println(huy.replace(huy.substring(0,4), ""));
         System.out.println(huy);
+        Scanner input = new Scanner(System.in);
     }
 }
